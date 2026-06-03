4bdb9de4f5f0945f9b601f073184f708e964a3fe
Marcio R. Silva <mr3475995@gmail.com>
2026-06-03 14:50:39 -0300

    Criação de cores e onClick button e icone do aplicativo

--- app/src/main/java/com/marcioposgraduacao/trucogamemarker/MainActivity.kt
+++ app/src/main/java/com/marcioposgraduacao/trucogamemarker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.marcioposgraduacao.trucogamemarker
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun btnMatchHistory(view: View) {}
+    fun btnResetHistory(view: View) {}
+    fun btnSetNames(view: View) {}
 }
